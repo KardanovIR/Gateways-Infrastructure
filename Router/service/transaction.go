9502b8553813989110de3e5708777a04e7dfc6bf
sDrynkina <sdrynkina@wavesplatform.com>
2019-04-08 11:10:19 +0300

    [GW-69] Nginx configuration. Change navigation's rules to avoid service type in header's value

--- Router/service/transaction.go
+++ Router/service/transaction.go
@@ -11,15 +11,14 @@ import (
 )
 
 const (
-	serviceNamePrefix = "adapter_"
-	headerName        = "blockchain-service"
+	headerName = "blockchain-type"
 )
 
 func (bs *blockchainsService) GetTransactionStatus(ctx context.Context, blockchain model.Blockchain, txID string) (string, error) {
 	log := logger.FromContext(ctx)
 	log.Debugf("call service method 'GetTransactionStatus' for %s, txID: %s", blockchain, txID)
 	requestCtx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs(headerName,
-		serviceNamePrefix+strings.ToLower(string(blockchain))))
+		strings.ToLower(string(blockchain))))
 	request := pb.GetTransactionStatusRequest{TxId: txID}
 	reply, err := bs.universal.GetTransactionStatus(requestCtx, &request)
 	if err != nil {
