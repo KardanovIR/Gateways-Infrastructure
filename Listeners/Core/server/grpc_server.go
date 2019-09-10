bf413b7b43a9fb8879612d07d6c58fe9e067ddc4
Arsenii Pechenkin <arspechen@gmail.com>
2019-09-10 21:20:18 +0300

    [GW-656] add removing task by tx hash

--- Listeners/Core/server/grpc_server.go
+++ Listeners/Core/server/grpc_server.go
@@ -17,6 +17,7 @@ import (
 type IGrpcServer interface {
 	AddTask(ctx context.Context, in *pb.AddTaskRequest) (*pb.AddTaskResponse, error)
 	RemoveTask(ctx context.Context, in *pb.RemoveTaskRequest) (*pb.Empty, error)
+	RemoveTaskByTxHash(ctx context.Context, in *pb.RemoveTaskByTxHashRequest) (*pb.Empty, error)
 }
 
 type grpcServer struct {
@@ -65,6 +66,27 @@ func (s *grpcServer) RemoveTask(ctx context.Context, in *pb.RemoveTaskRequest) (
 	if err != nil {
 		log.Errorf("Removing task fails: %s", err)
 		return nil, err
+	}
+
+	return &pb.Empty{}, nil
+}
+
+func (s *grpcServer) RemoveTaskByTxHash(ctx context.Context, in *pb.RemoveTaskByTxHashRequest) (*pb.Empty, error) {
+	log := logger.FromContext(ctx)
+	log.Info("RemoveTask")
+
+	tasks, err := s.rp.FindByAddressOrTxId(ctx, models.ChainType(in.Blockchain), "", in.Hash)
+	if err != nil && len(tasks) > 0 {
+		log.Errorf("Removing task fails: %s", err)
+		return nil, err
+	}
+
+	for _, task := range tasks {
+		err = s.rp.RemoveTask(ctx, task.Id)
+		if err != nil {
+			log.Errorf("Removing task fails: %s", err)
+			return nil, err
+		}
 	}
 
 	return &pb.Empty{}, nil
